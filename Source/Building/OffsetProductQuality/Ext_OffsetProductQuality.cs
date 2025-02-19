3446092830b0899e41083bed5f659d312c5a106a
MoonShine <ljhmyw@foxmail.com>
2025-02-20 00:06:48 +0800

    +工作台偏移作品品质

--- Source/Building/OffsetProductQuality/Ext_OffsetProductQuality.cs
+++ Source/Building/OffsetProductQuality/Ext_OffsetProductQuality.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
-namespace Astrologer.MapBuilding
+namespace Astrologer
 {
     public class Ext_OffsetProductQuality : DefModExtension
     {
--- Source/Building/OffsetProductQuality/Patch_OffsetProductQuality.cs
+++ Source/Building/OffsetProductQuality/Patch_OffsetProductQuality.cs
@@ -5,12 +5,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace Astrologer.HarmonyPatchs
 {
-    [HarmonyPatch(typeof(ResearchManager), "FinishProject")]
+    [HarmonyPatch(typeof(GenRecipe), "MakeRecipeProducts")]
     public class Patch_OffsetProductQuality
     {
+        [HarmonyPostfix]
+        public static IEnumerable<Thing> fix(IEnumerable<Thing> result, IBillGiver billGiver)
+        {
+            //bool shouldOffset = false;
+            Ext_OffsetProductQuality ext = null;
+            if (billGiver is Thing t)
+            {
+                ext = t.def.GetModExtension<Ext_OffsetProductQuality>();
+                //shouldOffset = true;
+            }
+            foreach (Thing thing in result)
+            {
+                if (ext != null)
+                {
+                    CompQuality compQuality = thing.TryGetComp<CompQuality>();
+                    compQuality?.SetQuality((QualityCategory)Mathf.Clamp((int)compQuality.Quality + ext.offsetAmout, (int)QualityCategory.Awful, (int)QualityCategory.Legendary), null);
+                }
+                yield return thing;
+            }
+        }
     }
 }
