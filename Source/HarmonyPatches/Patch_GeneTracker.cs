9a0bc6a524cd4ee52fce9695da8d6e4736ddaa49
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-02-23 20:34:20 +0800

    PawnKind和连衣裙

---
+++ Source/HarmonyPatches/Patch_GeneTracker.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Astrologer.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Pawn_GeneTracker), "Notify_GenesChanged")]
+    public static class Patch_GeneTracker
+    {
+        [HarmonyPostfix]
+        public static void Postfix_Notify_GenesChanged(ref Pawn ___pawn, ref GeneDef addedOrRemovedGene)
+        {
+            Ext_AstrologerGene ext = addedOrRemovedGene.GetModExtension<Ext_AstrologerGene>();
+            if (addedOrRemovedGene != null && ext != null && (ext.forcedBodyType != null))
+            {
+                ___pawn.story.bodyType = PawnGenerator.GetBodyTypeFor(___pawn);
+                ___pawn.Drawer.renderer.SetAllGraphicsDirty();
+            }
+        }
+    }
+}
