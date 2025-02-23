055b9f897f7c28e6c6155038d7e0dd008276bcc4
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-02-23 14:02:09 +0800

    Create Ext_AstroGeneBody.cs

---
+++ Source/Gene/Ext_AstroGeneBody.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Astrologer.Gene
+{
+    public class Ext_AstroGeneBody : DefModExtension
+    {
+        public bool isAstrologer = true;
+    }
+}
