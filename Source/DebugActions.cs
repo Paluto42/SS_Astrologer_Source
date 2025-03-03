c941d62b8a3d960a3a83d99ca6aa09f103dc4ea5
MoonShine <ljhmyw@foxmail.com>
2025-03-04 01:10:06 +0800

    小修

---
+++ Source/DebugActions.cs
@@ -0,0 +1,24 @@
+﻿using LudeonTK;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Astrologer
+{
+    public static class DebugActions
+    {
+        [DebugAction("Astro Actions", "OffsetPostionX-5", false, false, false, false, 0, false, allowedGameStates = AllowedGameStates.PlayingOnMap)]
+        public static void OffsetPositionX()
+        {
+            var t = Find.Selector.SelectedPawns?.First();
+            if (t != null)
+            {
+                t.Position = new IntVec3(t.Position.x + 5, t.Position.y, t.Position.z);
+            }
+        }
+    }
+}
