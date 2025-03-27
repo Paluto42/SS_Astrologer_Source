5288b8f7fb0cadec50bc886bf6fba8cb2bc8d1cd
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-03-27 22:11:29 +0800

    应急反应护服

---
+++ Source/Ability/AbilityEffect/AE_UnstableCatalyst.cs
@@ -0,0 +1,26 @@
+﻿using AKA_Ability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Astrologer.Ability
+{
+    public class AE_UnstableCatalyst : AbilityEffectBase
+    {
+        public int duration = 2500;
+
+        readonly string effect = EffectIDs.halfIgnoreDmg;
+
+        protected override bool DoEffect(AKAbility_Base caster, LocalTargetInfo target)
+        {
+            AstroDocument doc = caster.CasterPawn.TryGetAstroDoc();
+            if (doc.EffectValid(effect)) return false;
+            
+            doc.effects.Add(EffectIDs.halfIgnoreDmg, duration);
+            return base.DoEffect(caster, target);
+        }
+    }
+}
