73c86d60864a5075376f2f44f7158f74cb46d419
Paluto <85861331+Paluto42@users.noreply.github.com>
2025-04-09 01:11:15 +0800

    +失明时获得心情减溢, xml小修

--- Source/Gene/Gene_Astrologer.cs
+++ Source/Gene/Gene_Astrologer.cs
@@ -1,5 +1,7 @@
-﻿using AK_TypeDef;
-using Astrologer.Insight;
+﻿using AK_DLL;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
 using Verse;
 
 namespace Astrologer
@@ -7,6 +9,42 @@ namespace Astrologer
     //准备删了
     public class Gene_Astrologer : Gene
     {
+        private int tick = 0;
+
+        private bool ShouldRecover = false;
+
+        private int checkInterval = 250; //检测间隔
+
+        private int recoveryInterval = (int)TimeToTick.hour * 1; //失明后的回复周期
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref tick, "recoveryTick");
+        }
+
+        //眼睛会独立恢复
+        public override void Tick()
+        {
+            base.Tick();
+            if (pawn.Dead) return;
+
+            List<Hediff> blindness = null;
+            if (Utility.CrtTick % checkInterval == 0)
+            {
+                blindness = pawn.health.hediffSet.hediffs.FindAll(h => h.def == HediffDefOf.Blindness);
+                ShouldRecover = blindness.Any();
+            }
+            if (ShouldRecover) 
+            {
+                tick++;
+                if (tick % recoveryInterval == 0)
+                {
+                    blindness.ForEach(hediff => hediff.Severity -= 0.1f);
+                }
+            }
+        }
+
         public override void PostAdd()
         {
             /*Ext_ForcedAstrologer ext = def.GetModExtension<Ext_ForcedAstrologer>();
---
+++ Source/Thought/ThoughtWorker_SightMood.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace Astrologer
+{
+    //至少要有2个stages
+    public class ThoughtWorker_SightMood : ThoughtWorker
+    {
+        public int passiveStage = 0;
+        public int negativeStage = 1;
+
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            List<BodyPartRecord> parts = p.RaceProps.body.GetPartsWithDef(BodyPartDefOf.Eye);
+            if (parts.Any() == false) return ThoughtState.Inactive;
+
+            HediffSet hediffSet = p.health.hediffSet;
+            foreach (BodyPartRecord item in parts)
+            {
+                //眼球植入物,缺眼,失明都会不开心
+                if (hediffSet.PartOrAnyAncestorHasDirectlyAddedParts(item)
+                    || hediffSet.PartIsMissing(item) || hediffSet.HasHediff(HediffDefOf.Blindness))
+                {
+                    return ThoughtState.ActiveAtStage(negativeStage);
+                }
+            }
+            return ThoughtState.ActiveAtStage(passiveStage);
+        }
+    }
+}
